335c6fba25bd1e8409f766ac42caa14c23769291
FenixXx <fenix@urbanterror.info>
2013-11-11 12:42:50 +0100

    fixed typo in GPL header

--- extplugins/translator.py
+++ extplugins/translator.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 #
 # Plugin for BigBrotherBot(B3) (www.bigbrotherbot.net)
-# Copyright (C) 2013 Daniele Pantaleone <fenix@bigbrotherbot.net)
+# Copyright (C) 2013 Daniele Pantaleone <fenix@bigbrotherbot.net>
 # 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
